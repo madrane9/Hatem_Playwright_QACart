ab0b34d34195328048b0af4a9fc5544935086600
madrane9 <madrane9@hotmail.com>
2023-02-05 14:35:16 +0100

    Login using API weil es stable und schneller

--- utils/global-config.ts
+++ utils/global-config.ts
@@ -1,14 +1,18 @@
-import{chromium, expect} from '@playwright/test';
+import { request } from '@playwright/test';
+
 async function globalConfig() {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
-    await page.goto("https://qacart-todo.herokuapp.com/login");
-    await page.locator("#login").fill("test1234@test.de");
-    await page.locator('//input[@data-testid="password"]').fill("Test123");
-    await page.locator('button:has-text("Login")').click();
-    const welcomeMEssage = page.locator('[data-testid="welcome"]');
-    await expect(welcomeMEssage).toBeVisible();
-    await page.context().storageState({ path: 'storageState.json' })}
+    const requestContext = await request.newContext();
+    await requestContext.post("https://qacart-todo.herokuapp.com/api/v1/users/login", {
+        data: {
+            email: "test1234@test.de",
+            password:"Test123"
+        }
+   
+})
+
+
+}
+
   
 
 export default globalConfig;
